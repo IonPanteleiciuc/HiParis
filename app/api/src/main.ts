62fed51faf9d724fa724fe281eccddddfeedb391
Ion VPS <ionpanteleiciuc@gmail.com>
2023-12-02 16:46:44 +0100

    init app

--- app/api/src/main.ts
+++ app/api/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors({
+    credentials: true,
+    origin: ['http://vps-ion.tech', 'http://vps-ion.tech/predictix'],
+    methods: 'POST,PUT,DELETE,GET,PATCH',
+    allowedHeaders:
+      'Access-Control-Allow-Origin, Access-Control-Allow-Methods, Access-Control-Expose-Headers, Content-Type,Authorization',
+  });
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
+  await app.listen(3001);
 }
 bootstrap();
